de3311e5509b797ae083cadd82f5538f2cf5be10
VM2703 <volker.moritz@kiongroup.com>
2023-01-27 14:40:59 +0000

    commit now

---
+++ UX410Solutions/03_helloworld/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sap.training.helloworld.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ UX410Solutions/03_helloworld/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"saptraining/helloworld/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
